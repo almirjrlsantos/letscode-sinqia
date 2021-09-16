d37485fbf745a0b3429c46d815bd3c62f86e8359
Almir Junior de Lima Santos <ajr_almir@hotmail.com>
2021-09-16 17:56:41 -0300

    Testando atualização do git

--- src/br/com/letscode/java/aula.java
+++ src/br/com/letscode/java/aula.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class aula {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Numero: ");
         int num = scan.nextInt();
         limpar(scan);
+
+        System.out.print("Texto: ");
         String s = scan.nextLine();
+
         System.out.println(num);
         System.out.println(s);
+        scan.close();
     }
     public static void limpar(Scanner scan){
         if(scan.hasNextLine()){
